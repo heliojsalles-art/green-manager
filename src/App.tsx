80204a860b78f3b39d66ad8e43569517bc7571ca
heliojsalles-art <heliojsalles@gmail.com>
2026-03-11 13:20:09 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,45 @@
-import React, { useEffect } from 'react';
-import { IonApp, IonRouterOutlet, IonSplitPane, setupIonicReact } from '@ionic/react';
+import { Redirect, Route } from 'react-router-dom';
+import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { Route } from 'react-router-dom';
 
-// Importar páginas
+// Importe as páginas (ajuste os caminhos conforme necessário)
 import Home from './pages/Home';
+import Finances from './pages/Finances';
 import Reminders from './pages/Reminders';
 import ShoppingLists from './pages/ShoppingLists';
-import Finances from './pages/Finances';
-import Settings from './pages/Settings';
 
-// Importar componentes
-import Menu from './components/common/Menu';
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
 
-// Importar serviços
-import { DatabaseService } from './services/database';
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
 
 setupIonicReact();
 
 const App: React.FC = () => {
-  useEffect(() => {
-    const initApp = async () => {
-      const db = DatabaseService.getInstance();
-      await db.initDB();
-      
-      // Configurar limpeza automática (24h)
-      setInterval(() => {
-        db.cleanupCompletedItems();
-      }, 60 * 60 * 1000); // A cada hora
-    };
-    
-    initApp();
-  }, []);
-
   return (
     <IonApp>
       <IonReactRouter>
-        <IonSplitPane contentId="main">
-          <Menu />
-          <IonRouterOutlet id="main">
-            <Route exact path="/" component={Home} />
-            <Route exact path="/reminders" component={Reminders} />
-            <Route exact path="/shopping" component={ShoppingLists} />
-            <Route exact path="/finances" component={Finances} />
-            <Route exact path="/settings" component={Settings} />
-          </IonRouterOutlet>
-        </IonSplitPane>
+        <IonRouterOutlet>
+          {/* REMOVIDO: exact={true} de todas as rotas */}
+          <Route path="/" component={Home} />
+          <Route path="/finances" component={Finances} />
+          <Route path="/reminders" component={Reminders} />
+          <Route path="/shopping-lists" component={ShoppingLists} />
+        </IonRouterOutlet>
       </IonReactRouter>
     </IonApp>
   );
